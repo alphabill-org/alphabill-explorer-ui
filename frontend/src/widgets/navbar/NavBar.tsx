f99d306a2c1cb576fab1c02db197d07eae010613
Igor Mahlinovski <igmahl@ttu.ee>
2023-10-25 02:29:41 +0300

    Optimize mobile-screen menu. Change style.

--- frontend/src/widgets/navbar/NavBar.tsx
+++ frontend/src/widgets/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import logoImg from "../../shared/ui/assets/logo/logo.png";
@@ -28,12 +28,22 @@ const NavBar = () => {
   };
 
   const overlayClass = isOpen
-    ? "fixed inset-0 bg-indigo-700 z-50"
+    ? "fixed inset-0 z-50"
     : "flex flex-col";
+
+  useEffect(() => {
+    const closeMenu = () => {
+      setIsOpen(false);
+    };
+
+    window.addEventListener('resize', closeMenu);
+
+    return () => window.removeEventListener('resize', closeMenu);
+  }, [isOpen]);
 
   return (
     <div className={overlayClass}>
-      <nav className=" bg-indigo-700">
+      <nav className=" bg-[#4e3fb6]">
         <div className=" container max-w-6xl mx-auto justify-end hidden sm:flex space-x-4 px-4 py-1">
           {subMenu.map((item) => (
             <Link className=" text-white " to={item.link}>
@@ -45,7 +55,7 @@ const NavBar = () => {
           </a>
         </div>
       </nav>
-      <nav className="bg-slate-900">
+      <nav className={`${isOpen ? "bg-[#4e3fb6]" : "bg-[#0c0a3e]"} transition-all duration-100`}>
         <div className="container max-w-6xl mx-auto flex justify-between items-center p-4">
           <div className="flex items-center">
             <div className="text-white text-xl font-semibold">
@@ -73,23 +83,33 @@ const NavBar = () => {
 
         {isOpen && (
           <div className="sm:hidden flex flex-col">
-            {menu.map((item) => (
-              <Link className=" text-white px-4 py-2" to={item.link}>
-                {item.title}
-              </Link>
-            ))}
+            <div className="flex flex-col">
+              {menu.map((item) => (
+                <Link onClick={toggleMenu} className=" text-white px-4 py-2" to={item.link}>
+                  {item.title}
+                </Link>
+              ))}
 
-            {subMenu.map((item) => (
-              <Link
-                className="bg-indigo-700 px-4 py-2 text-white"
-                to={item.link}
-              >
-                {item.title}
-              </Link>
-            ))}
-            <a className=" bg-indigo-700" href="#">
-              <img src={IconDiscord} alt="Discord" className="" />
-            </a>
+              {subMenu.map((item) => (
+                <Link
+                  className="px-4 py-2 text-white"
+                  to={item.link}
+                >
+                  {item.title}
+                </Link>
+              ))}
+
+              <span className="h-[2px] bg-black bg-opacity-20 mr-4 ml-4 my-2"></span>
+
+              <div className="flex flex-row px-4 py-2 mb-2 justify-between">
+                <div className="flex flex-row">
+                  <p className=" font-medium text-white"> ALPHA: 100$</p>
+                  <p className="text-[#08e8de] ml-1">(+1.06%)</p>
+                </div>
+
+                <p className=" font-medium text-white"> TxFee: 1 ALPHA</p>
+              </div>
+            </div>
           </div>
         )}
       </nav>
