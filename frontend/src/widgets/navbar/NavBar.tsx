385304941bc643563ad6c7bb2397ca1582167b93
Igor Mahlinovski <igmahl@ttu.ee>
2023-10-26 15:39:26 +0300

    Add social networks icons Close mobile menu on outside click Close mobile menu on screen size > 640px Optimize useEffects Add hover and active effects to navbar links

--- frontend/src/widgets/navbar/NavBar.tsx
+++ frontend/src/widgets/navbar/NavBar.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import { NavLink } from "react-router-dom";
 
 import logoImg from "../../shared/ui/assets/logo/logo.png";
-import IconDiscord from "../../shared/ui/assets/icons/Ico_Discord.png";
+import { IconSocialDiscord } from "../../shared/ui/icons";
 
 type Menu = {
   title: string;
@@ -22,9 +22,14 @@ const menu: Menu[] = [
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const mobileMenuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
   };
 
   const overlayClass = isOpen
@@ -32,30 +37,44 @@ const NavBar = () => {
     : "flex flex-col";
 
   useEffect(() => {
-    const closeMenu = () => {
-      setIsOpen(false);
-    };
+    const handleResize = () => {
+      if (window.innerWidth >= 640) {
+        closeMenu();
+      }
+    }
 
-    window.addEventListener('resize', closeMenu);
+    window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener('resize', closeMenu);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+
+  useEffect(() => {
+    const closeMenuByClick = (event: MouseEvent) => {
+      if (isOpen && !mobileMenuRef.current?.contains(event.target as Node)) {
+        closeMenu();
+      }
+    }
+
+    window.addEventListener('click', closeMenuByClick);
+
+    return () => window.removeEventListener('click', closeMenuByClick);
+
   }, [isOpen]);
 
   return (
     <div className={overlayClass}>
       <nav className=" bg-[#4e3fb6]">
-        <div className=" container max-w-6xl mx-auto justify-end hidden sm:flex space-x-4 px-4 py-1">
+        <div className=" container max-w-6xl mx-auto justify-end items-center hidden sm:flex space-x-4 px-4 py-1">
           {subMenu.map((item) => (
-            <Link className=" text-white " to={item.link}>
+            <NavLink className={({ isActive }) => isActive ? "text-[#08e8de] font-medium" : "text-white hover:text-[#08e8de] transition-colors duration-300"} to={item.link}>
               {item.title}
-            </Link>
+            </NavLink>
           ))}
-          <a href="#">
-            <img src={IconDiscord} alt="Discord" className="" />
-          </a>
+          <IconSocialDiscord className="fill-white hover:fill-[#08e8de] transition-colors duration-300"/>
         </div>
       </nav>
-      <nav className={`${isOpen ? "bg-[#4e3fb6]" : "bg-[#0c0a3e]"} transition-all duration-100`}>
+      <nav ref={mobileMenuRef} className={`${isOpen ? "bg-[#4e3fb6]" : "bg-[#0c0a3e]"} transition-all duration-100`}>
         <div className="container max-w-6xl mx-auto flex justify-between items-center p-4">
           <div className="flex items-center">
             <div className="text-white text-xl font-semibold">
@@ -65,9 +84,9 @@ const NavBar = () => {
 
           <div className="hidden sm:flex space-x-4">
             {menu.map((item) => (
-              <Link className=" text-white" to={item.link}>
+              <NavLink className={({ isActive }) => isActive ? "text-[#08e8de] font-medium" : "text-white hover:text-[#08e8de] transition-colors duration-300"} to={item.link}>
                 {item.title}
-              </Link>
+              </NavLink>
             ))}
           </div>
 
@@ -85,18 +104,15 @@ const NavBar = () => {
           <div className="sm:hidden flex flex-col">
             <div className="flex flex-col">
               {menu.map((item) => (
-                <Link onClick={toggleMenu} className=" text-white px-4 py-2" to={item.link}>
+                <NavLink onClick={toggleMenu} className={({ isActive }) => isActive ? "text-[#08e8de] px-4 py-2" : "px-4 py-2 text-white hover:bg-indigo-800"} to={item.link}>
                   {item.title}
-                </Link>
+                </NavLink>
               ))}
 
               {subMenu.map((item) => (
-                <Link
-                  className="px-4 py-2 text-white"
-                  to={item.link}
-                >
+                <NavLink onClick={toggleMenu} className={({ isActive }) => isActive ? "text-[#08e8de] px-4 py-2" : "px-4 py-2 text-white hover:bg-indigo-800"} to={item.link}>
                   {item.title}
-                </Link>
+                </NavLink>
               ))}
 
               <span className="h-[2px] bg-black bg-opacity-20 mr-4 ml-4 my-2"></span>
