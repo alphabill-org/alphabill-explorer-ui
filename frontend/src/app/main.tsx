097cdc4c4418fbf530f999ea330d83e9a10dc8e2
Igor <igor.mahlinovski@gmail.com>
2023-11-10 00:12:00 +0200

    Add TanStack Query

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./providers/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
