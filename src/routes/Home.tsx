c04fe5bcb79ac19c2b81c11f42c24d23cf830af4
Robert Pihelgas <robert.pihelgas@guardtime.com>
2025-03-04 16:08:11 +0200

    Update favicon, logo, and title; add robots.txt

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -22,7 +22,7 @@ export const Home: React.FC = () => {
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl font-bold text-center mt-8">
-        Alphabill Block Explorer
+        Alphabill Explorer
       </h1>
 
       <Search />
