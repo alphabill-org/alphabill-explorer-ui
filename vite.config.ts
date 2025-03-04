d26734668858af8a99fb5a2fc63e947527524d87
Robert Pihelgas <robert.pihelgas@guardtime.com>
2025-03-04 22:00:44 +0200

    Add missing tailwindcss dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
